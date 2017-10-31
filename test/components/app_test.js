d2724a869fe82f0200b607c3de997ca55a52f0c5
gogolearn <jay.jongcheol.park@gmail.com>
2017-10-31 19:45:01 -0400

    Simulating events

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -3,14 +3,13 @@ import App from '../../src/components/app'
 
 // Use 'describe' to group together similar tests
 describe('App', () => {
-  
-  // Use 'it' to test a single attribute of a target
-  it('shows the correct text', () => {
-    
-    // Create an instance of App
-    const component = renderComponent(App)
+  let component
 
-    // Use 'expect' to make an 'assertion' about a target
-    expect(component).to.contain('React simple starter')
+  beforeEach(() => {
+    component = renderComponent(App)
+  })
+
+  it('shows a comment box', () => {
+    expect (component.find('.comment-box')).to.exist
   })
 })
