f67eafce29f2ce4d34356b749943a78eb1336150
gogolearn <jay.jongcheol.park@gmail.com>
2017-10-31 22:06:16 -0400

    Form Submit Event

--- src/components/comment_box.js
+++ src/components/comment_box.js
@@ -8,15 +8,21 @@ class CommentBox extends Component {
     this.setState({ comment: event.target.value })
   }
 
+  handleSubmit = event => {
+    event.preventDefault()
+
+    this.setState({ comment: '' })
+  }
+
   render () {
     return (
-      <div className="comment-box">
+      <form onSubmit={this.handleSubmit} className="comment-box">
         <textarea
           value={this.state.comment}
           onChange={this.handleChange}
         />
         <button>Submit Comment</button>
-      </div>
+      </form>
     )
   }
 }
